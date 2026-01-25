354d29a8aa1d0a416d26a16219c1c9244d318fa7
Nithin <nithinteja.43@gmail.com>
2026-01-25 21:40:11 +0530

    Visual testing and API request intercept

--- src/PageObjects/OrderHistoryPage.ts
+++ src/PageObjects/OrderHistoryPage.ts
@@ -3,44 +3,45 @@ import { IOrderHistory } from "@src/interfaces/IOrderHistory";
 import { orderHistorySelectors } from "@src/pages/selectors/orderHistory";
 
 export class OrderHistoryPage {
-    readonly page: Page;
-    public locators: IOrderHistory;
-    constructor(page: Page) {
-        this.page = page;
-        this.locators = {} as IOrderHistory;
-        for (const key of Object.keys(orderHistorySelectors) as Array<keyof typeof orderHistorySelectors>) {
-            const selector = orderHistorySelectors[key];
-            this.locators[key] = this.page.locator(selector);
-        }
+  readonly page: Page;
+  public locators: IOrderHistory;
+  constructor(page: Page) {
+    this.page = page;
+    this.locators = {} as IOrderHistory;
+    for (const key of Object.keys(orderHistorySelectors) as Array<
+      keyof typeof orderHistorySelectors
+    >) {
+      const selector = orderHistorySelectors[key];
+      this.locators[key] = this.page.locator(selector);
     }
-    async validateOrderHistoryHeaderText(text:string) {
-        expect(await this.locators.getHeader.textContent()).toBe(text);
-    }
+  }
+  async validateOrderHistoryHeaderText(text: string) {
+    expect(await this.locators.getHeader.textContent()).toBe(text);
+  }
 
-    async validateOrderIdInOrderHistory(orderId:string, orderIdIndex: number) {
-        const orderIds = this.locators.getOrderIdList;
-        expect(await orderIds.nth(orderIdIndex).textContent()).toBe(orderId);
-    }
-    async getOrderIdIndex(orderId: string): Promise<number> {
-        const orderIds = this.locators.getOrderIdList;
-        const count = await orderIds.count();
-        let found = false;
+  async validateOrderIdInOrderHistory(orderId: string, orderIdIndex: number) {
+    const orderIds = this.locators.getOrderIdList;
+    expect(await orderIds.nth(orderIdIndex).textContent()).toBe(orderId);
+  }
+  async getOrderIdIndex(orderId: string): Promise<number> {
+    const orderIds = this.locators.getOrderIdList;
+    const count = await orderIds.count();
+    let found = false;
 
-        for (let i = 0; i < count; i++) {
-            const currentOrderId = await orderIds.nth(i).textContent();
-            if (currentOrderId?.trim() === orderId) {
-                found = true;
-                return i;
-            }
-        }
-        throw new Error(`Order ID ${orderId} not found in order history.`);
+    for (let i = 0; i < count; i++) {
+      const currentOrderId = await orderIds.nth(i).textContent();
+      if (currentOrderId?.trim() === orderId) {
+        found = true;
+        return i;
+      }
     }
-    async getOrderDetails(orderId:string,orderIdIndex:number){
-        const orderIds = this.locators.getOrderIdList;
-        const orderDetails = this.locators.getTableRows.locator('.btn-primary');
-        expect(await orderIds.nth(orderIdIndex).textContent()).toBe(orderId);
-        await orderDetails.nth(orderIdIndex).click();
-        await this.page.waitForLoadState('networkidle');
-        
-    }
+    throw new Error(`Order ID ${orderId} not found in order history.`);
+  }
+  async getOrderDetails(orderId: string, orderIdIndex: number) {
+    const orderIds = this.locators.getOrderIdList;
+    const orderDetails = this.locators.getTableRows.locator(".btn-primary");
+    expect(await orderIds.nth(orderIdIndex).textContent()).toBe(orderId);
+    await orderDetails.nth(orderIdIndex).click();
+    await this.page.waitForLoadState("networkidle");
+  }
 }
